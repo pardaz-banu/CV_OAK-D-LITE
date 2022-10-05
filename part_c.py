6de44e6621b052173d14a76675805de8f8580269
pardaz-banu <45086957+pardaz-banu@users.noreply.github.com>
2022-10-05 17:52:24 -0400

    Update part_c.py

--- part_c.py
+++ part_c.py
@@ -1,85 +1,69 @@
-from inspect import iscoroutinefunction
 import cv2
-import numpy as np
 import depthai as dai
-from unicodedata import name
-from operator import imod
-#function to extract fram
-def getFrame(q):
-    frame = q.get()
-    return frame.getCvFrame() #converting the frame to openCV formate and then returning it
+import numpy as np
 
-#function to select mono camera
-def getMono(pipe, isLeft):
-    mono = pipe.createMonoCamera()
-    mono.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-    if isLeft:
-        mono.setBoardSocket(dai.CameraBoardSocket.LEFT)
-    else:
-        mono.setBoardSocket(dai.CameraBoardSocket.RIGHT)
-    return mono
+# Start defining a pipeline
+pipeline = dai.Pipeline()
 
-def getStero(pipe, left, right):
-    stereo = pipe.createStereoDepth()
-    stereo.setLeftRightCheck(True)
-    left.out.link(stereo.left)
-    right.out.link(stereo.right)
-    return  stereo
+#Define a source - color camera
+cam_rgb = pipeline.createColorCamera()
+cam_rgb.setPreviewSize(750, 750)
+cam_rgb.setBoardSocket(dai.CameraBoardSocket.RGB)
+cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+cam_rgb.setInterleaved(False)
+cam_rgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
 
-pipe = dai.Pipeline()
+# Create output
+xout_rgb = pipeline.createXLinkOut()
+xout_rgb.setStreamName("rgb")
+cam_rgb.preview.link(xout_rgb.input)
 
-xLeft = pipe.createXLinkOut()
-xLeft.setStreamName("Left")
+# Define a source - two mono (grayscale) cameras
+left = pipeline.createMonoCamera()
+left.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+left.setBoardSocket(dai.CameraBoardSocket.LEFT)
 
-xRight = pipe.createXLinkOut()
-xRight.setStreamName("Right")
+right = pipeline.createMonoCamera()
+right.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+right.setBoardSocket(dai.CameraBoardSocket.RIGHT)
 
-monoLeft = getMono(pipe, isLeft=True)
-monoRight = getMono(pipe, isLeft=False)
-stereo = getStero(pipe, monoLeft, monoRight)
-xout_left = pipe.createXLinkOut()
-xout_left.setStreamName("leftrectify")
-xout_right = pipe.createXLinkOut()
-xout_right.setStreamName("rightrectify")
-xout_display = pipe.createXLinkOut()
-xout_display.setStreamName("Disparity")
-stereo.disparity.link(xout_display.input)
+# Create a node that will produce the depth map (using disparity output as it's easier to visualize depth this way)
+depth = pipeline.createStereoDepth()
+depth.setConfidenceThreshold(200)
+left.out.link(depth.left)
+right.out.link(depth.right)
 
-stereo.rectifiedLeft.link(xout_left.input)
-stereo.rectifiedRight.link(xout_right.input)
 
-with dai.Device(pipe) as device:
-    disparity_queue = device.getOutputQueue(name = "Disparity", maxSize = 1)
-    
-    leftQ = device.getOutputQueue(name = "Left", maxSize = 1)
-    rightQ = device.getOutputQueue(name = 'Right', maxSize = 1)
-    # Calculate a multiplier for color mapping disparity map
-    disparityMultiplier = 255 / stereo.initialConfig.getMaxDisparity()
-    cv2.namedWindow("Stereo Pair")
-    count = 1
-    multiplier = 255/stereo.initialConfig.getMaxDisparity()
+# Create output
+xout = pipeline.createXLinkOut()
+xout.setStreamName("disparity")
+depth.disparity.link(xout.input)
+
+# Pipeline defined, now the device is connected to
+with dai.Device(pipeline) as device:
+    # Start pipeline
+    device.startPipeline()
+
+    # Output queue will be used to get the rgb frames from the output defined above
+    q_rgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+
+    # Output queue will be used to get the disparity frames from the outputs defined above
+    q = device.getOutputQueue(name="disparity", maxSize=4, blocking=False)
+
     while True:
-        leftFrame = getFrame(leftQ)
-        rightFrame = getFrame(rightQ)
-        disparity = (disparity*multiplier).astype(np.uint8)
-        imOut = np.uint8(leftFrame/2 + rightFrame/2)
-        imOut = cv2.cvtColor(imOut, cv2.COLOR_GRAY2RGB)
+        in_rgb = q_rgb.get()  # blocking call, will wait until a new data has arrived
 
-        cv2.imshow("Stereo Pair", imOut)
-        cv2.imshow("Disparity", disparity)
+        # Retrieve 'bgr' (opencv format) frame
+        cv2.imshow("bgr", in_rgb.getCvFrame())
 
-        k = cv2.waitKey(1)
-        if k%256 ==27:
-            #Esc pressed
-            print("Escape hit, closing operation")
+        in_depth = q.get()  # blocking call, will wait until a new data has arrived
+        # data is originally represented as a flat 1D array, it needs to be converted into HxW form
+        frame = in_depth.getData().reshape((in_depth.getHeight(), in_depth.getWidth())).astype(np.uint8)
+        frame = np.ascontiguousarray(frame)
+        # frame is transformed, the color map will be applied to highlight the depth info
+        frame = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
+        # frame is ready to be shown
+        cv2.imshow("disparity", frame)
+
+        if cv2.waitKey(1) == ord('q'):
             break
-        elif k%256 == 32:
-            #Space pressed
-            img_name = f"opencv_picture_{count}.png"
-            cv2.imwrite(img_name, imOut)
-            print(f"{img_name} saved")
-            count += 1
-
-cv2.destroyAllWindows()
-
-
